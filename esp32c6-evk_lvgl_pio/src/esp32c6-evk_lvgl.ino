e694205592584ca8900fda03a5db9a6cd9a789cf
David <dbn@b4e.biz>
2025-08-26 16:41:20 +0200

    Added correct partition.csv, removed unnecessary flags in platformio.ini

--- esp32c6-evk_lvgl_pio/src/esp32c6-evk_lvgl.ino
+++ esp32c6-evk_lvgl_pio/src/esp32c6-evk_lvgl.ino
@@ -1,17 +1,101 @@
+#include <Arduino.h>
 #include "GuiManager.h"
 #include "ui.h"
 #include "custom_actions.h"
+#include "Zigbee.h"
+
+#define TEMP_SENSOR_ENDPOINT_NUMBER 1
+TaskHandle_t tsk_temp_task;
+
+// Optional Time cluster variables
+struct tm timeinfo;
+struct tm *localTime;
+int32_t timezone;
+
+// zbTempSensor simple temperature sensor
+ZigbeeTempSensor zbTempSensor = ZigbeeTempSensor(TEMP_SENSOR_ENDPOINT_NUMBER);
 
 GuiManager gui;
 
+/************************ Temp sensor *****************************/
+static void temp_sensor_value_update(void *arg) {
+  for (;;)
+  {
+    // call sensors.requestTemperatures() to issue a global temperature
+    // request to all devices on the bus
+    Serial.print("Requesting temperatures...");
+
+    zbTempSensor.setTemperature(12.34);
+
+    // Read temperature sensor value
+    //float tsens_value = temperatureRead();
+    //Serial.printf("Updated temperature sensor value to %.2f°C\r\n", tsens_value);
+    // Update temperature value in Temperature sensor EP
+    //zbTempSensor.setTemperature(tsens_value);
+    
+    delay(1000);
+  }
+}
+
+
 void setup() {
     Serial.begin(115200);
+    Serial.printf("Arduino Stack was set to %d bytes", getArduinoLoopTaskStackSize());
+    Serial.printf("\nSetup() - Free Stack Space: %d", uxTaskGetStackHighWaterMark(NULL));
+
+    // Optional: set Zigbee device name and model
+    zbTempSensor.setManufacturerAndModel("B4E SRL", "Zigbee-lvgl-1");
+    zbTempSensor.setMinMaxValue(10, 50);
+    zbTempSensor.setTolerance(0.1);
+    zbTempSensor.addTimeCluster();
+    zbTempSensor.setPowerSource(ZB_POWER_SOURCE_MAINS);
+
+
+    Zigbee.addEndpoint(&zbTempSensor);
+
+  Serial.println("Starting Zigbee...");
+  // When all EPs are registered, start Zigbee in Router Device mode
+  if (!Zigbee.begin(ZIGBEE_ROUTER)) {
+    Serial.println("Zigbee failed to start!");
+    Serial.println("Rebooting...");
+    ESP.restart();
+  } else {
+    Serial.println("Zigbee started successfully!");
+  }
+  
+ #if 1
+  Serial.println("Connecting to network");
+  while (!Zigbee.connected()) {
+    Serial.print(".");
+    delay(100);
+  }
+  Serial.println("Connected");
+#endif
+
+  // Optional: If time cluster is added, time can be read from the coordinator
+  timeinfo = zbTempSensor.getTime();
+  timezone = zbTempSensor.getTimezone();
+
+  Serial.println("UTC time:");
+  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+
+  time_t local = mktime(&timeinfo) + timezone;
+  localTime = localtime(&local);
+
+  Serial.println("Local time with timezone:");
+  Serial.println(localTime, "%A, %B %d %Y %H:%M:%S");
+
+    // Start Temperature sensor reading task
+  xTaskCreate(temp_sensor_value_update, "temp_sensor_update", 4096, NULL, 10, NULL);
+
     gui.begin();
 
     // --- UI ---
     ui_init();
 
     gui.startLvglTask();
+
+
     Serial.println("Setup done");
 }
 
